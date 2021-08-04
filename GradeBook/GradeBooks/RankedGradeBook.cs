2eda0cde4752f5d2af5ec337843915a2e5f2a3cb
Tyrone Wong <tyronewong@microsoft.com>
2021-08-04 16:19:28 -0700

    Completed module 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(this.Students.Count < 5)
+            {
+                throw new InvalidOperationException("Error less than 5 students");
+            }
+            
+            if(averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
